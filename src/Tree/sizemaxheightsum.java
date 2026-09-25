d477abb8e239fd4ded63df71174aa1fc8f527b6d
AmanPatidar8085 <patidaraman444@gmail.com>
2026-09-25 15:02:00 +0530

    min/max

--- src/Tree/sizemaxheightsum.java
+++ src/Tree/sizemaxheightsum.java
@@ -30,6 +30,34 @@ public class sizemaxheightsum {
 		if(root==null)return 0;
 		return  root.val+sum(root.left)+sum(root.right);
 	}
+	//max element
+	static int max(Node root) {
+		if(root==null) return Integer.MIN_VALUE;
+		int a=root.val;
+		int b=max(root.left);
+		int c=max(root.right);
+		return Math.max(a, Math.max(b, c));
+	}
+	//     find height
+	static int height(Node root) {
+		if(root==null)return 0;
+		if(root.left==null||root.right==null)return 0;
+		return 1+Math.max(height(root.left),height(root.right));
+	}
+	//product
+	static int prod(Node root) {
+		if(root==null)return 1;
+		return root.val*prod(root.left)*prod(root.right);
+	}
+	//minimum
+	
+	static int min(Node root) {
+		if(root==null) return Integer.MAX_VALUE;
+		int a=root.val;
+		int b=min(root.left);
+		int c=min(root.right);
+		return Math.min(a, Math.min(b, c));
+	}
 	public static void main(String[] args) {
 		Node root = new Node(10);
 //		System.out.println(root.val);
@@ -52,6 +80,9 @@ public class sizemaxheightsum {
 		preorder(root);
 		System.out.println("size of tree --> "+size(root));
 		System.out.println("sum of all element present in tree-->  "+sum(root));
-	
+		System.out.println("maximum ele of tree --> "+max(root));
+		System.out.println("height of tree-->"+height(root));
+		System.out.println("product of tree -->"+prod(root));
+		System.out.println("min ele of tree-->"+ min(root));
 }
 }
